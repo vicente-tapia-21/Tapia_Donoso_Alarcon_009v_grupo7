d0de84bed12f2557245e00600248c9e0eebedea7
andrés <dresanbeats@gmail.com>
2026-05-05 21:50:22 -0400

    proyecto KioskoDonPepe

---
+++ KioscoDonPepe/src/test/java/com/KioscoDonPepe/KioscoDonPepe/KioscoDonPepeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KioscoDonPepe.KioscoDonPepe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioscoDonPepeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
