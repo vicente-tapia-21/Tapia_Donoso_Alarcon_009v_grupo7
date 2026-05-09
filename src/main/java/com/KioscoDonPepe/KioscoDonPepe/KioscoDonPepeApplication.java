b018de018e5db9790d02300a57dca778025d4d55
vicente-tapia-21 <vice.tapiap@duocuc.cl>
2026-05-08 21:27:22 -0400

    nada

---
+++ src/main/java/com/KioscoDonPepe/KioscoDonPepe/KioscoDonPepeApplication.java
@@ -0,0 +1,13 @@
+package com.KioscoDonPepe.KioscoDonPepe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioscoDonPepeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioscoDonPepeApplication.class, args);
+	}
+
+}
